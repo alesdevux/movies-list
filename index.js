7dc1fa6b3062f86493529a20bedf0dc53e8b081b
Ales <aleswebgit@gmail.com>
2022-03-08 15:40:31 +0100

    add images, and style view in background movie

--- index.js
+++ index.js
@@ -14,13 +14,25 @@ fetch('db.json')
 function viewMovies(movies) {
   for (let i = 0; i < movies.length; i++) {
     const movie = movies[i]
+    const id = "movie-" + movie.id
+
+    if (movie.img === "" || movie.img === null || movie.img === undefined) {
+      movie.img = "default.png"
+    }
+
     domMovies.innerHTML += `
-      <div class="movie">
-        <h2>${movie.title}</h2>
-        <p>${movie.director}</p>
-        <p>${movie.category}</p>
-        <p>${movie.starts}</p>
+      <div class="movie" id="${id}">
+        <div class="bg-filter">
+          <h2>${movie.title}</h2>
+          <div class="movie-info">
+            <p>${movie.director}</p>
+            <p>${movie.category}</p>
+            <p>${movie.starts}</p>
+          </div>
+        </div>
       </div>
     `
+    const domMovie = document.getElementById(id)
+    domMovie.style.backgroundImage = `url("./assets/img/${movie.img}")`
   }
 }
