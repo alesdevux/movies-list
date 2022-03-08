3ac343287b135bfecb585de8b660a8a31dc8cb64
Ales <aleswebgit@gmail.com>
2022-03-08 16:10:29 +0100

    add and style stars of movies

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ function viewMovies(movies) {
       movie.img = "default.png"
     }
 
+    // add star.svg for movie.stars
+    let stars = ``
+    for (let j = 0; j < movie.stars; j++) {
+      stars += `<img src="./assets/svg/star.svg" alt="star" class="star">`
+    }
+    for (let x = movie.stars; x < 5; x++) {
+      stars += `<img src="./assets/svg/star-outline.svg" alt="star-outline" class="star-outline">`
+    }
+    console.log(stars)
+
     domMovies.innerHTML += `
       <div class="movie" id="${id}">
         <div class="bg-filter">
@@ -27,7 +37,10 @@ function viewMovies(movies) {
           <div class="movie-info">
             <p>${movie.director}</p>
             <p>${movie.category}</p>
-            <p>${movie.starts}</p>
+            <div class="stars">
+              ${stars}
+              <span>${movie.stars}</span>
+            </div>
           </div>
         </div>
       </div>
